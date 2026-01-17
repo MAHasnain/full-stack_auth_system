a3ae37c6300bc952c0dee3de23c9321957cae7e8
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2026-01-18 00:30:26 +0500

    update: env variables add and config file update proxy update

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,21 @@
 type config = {
   mongodb_uri: string;
   token_secret: string;
+  mailtrap_token: string;
+  mailtrap_host: string;
+  mailtrap_port: number;
+  mailtrap_username: string;
+  mailtrap_password: string;
+  domain: string;
 };
 
 export const config = {
   mongodb_uri: process.env.MONGODB_URI,
   token_secret: process.env.TOKEN_SECRET,
+  mailtrap_token: process.env.MAILTRAP_TOKEN,
+  mailtrap_host: process.env.MAILTRAP_HOST,
+  mailtrap_port: process.env.MAILTRAP_PORT,
+  mailtrap_username: process.env.MAILTRAP_USERNAME,
+  mailtrap_password: process.env.MAILTRAP_PASSWORD,
+  domain: process.env.DOMAIN
 };
--- src/proxy.ts
+++ src/proxy.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from "next/server";
 // This function can be marked `async` if using `await` inside
 export function proxy(request: NextRequest) {
   const path = request.nextUrl.pathname;
-  const isPublicPath = path === "/login" || path === "/signup";
+  const isPublicPath = path === "/login" || path === "/signup" || path === "/verifyemail"
 
   const token = request.cookies.get("token")?.value || "";
 
@@ -22,5 +22,5 @@ export function proxy(request: NextRequest) {
 // export default function proxy(request: NextRequest) { ... }
 
 export const config = {
-  matcher: ["/", "/profile", "/login", "/signup"],
+  matcher: ["/", "/profile", "/login", "/signup", '/verifyemail'],
 };
